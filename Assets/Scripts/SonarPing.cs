15da46a33952945d0fd992b53a66275c3605258d
Marek Baczynski <marek@superhotgame.com>
2017-01-20 21:25:53 +0100

    Initial commit of sonar script, simple sonar test scene

---
+++ Assets/Scripts/SonarPing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarPing : MonoBehaviour {
+
+	public GameObject sonarPointPrefab;
+
+	public int pointCount;
+
+	void sonar(){
+		var me = Camera.main.transform;
+		RaycastHit hit;
+
+		for (var x = 0; x < Camera.main.pixelWidth; x += 20) {
+			for (var y = 0; y < Camera.main.pixelHeight; y += 20) {
+				var ray = Camera.main.ScreenPointToRay (new Vector3 (x, y, 0f));
+				if (Physics.Raycast (ray, out hit)) {
+					var sonarPoint = GameObject.Instantiate (sonarPointPrefab, hit.point, Quaternion.identity);
+					sonarPoint.GetComponent<SonarPointFadeIn> ().timeout = hit.distance;
+					sonarPoint.transform.rotation = Quaternion.LookRotation (hit.normal);
+					Debug.DrawRay (hit.point, hit.normal*0.1f, Color.magenta, 5f);
+					pointCount++;
+				}
+			}
+		}
+
+		Debug.Log (pointCount);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			sonar ();
+		}
+	}
+}
---
+++ Assets/Scripts/SonarPointFadeIn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarPointFadeIn : MonoBehaviour {
+
+	public float timeout;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeout -= speed;
+		if (timeout < 0f) {
+			this.GetComponent<SpriteRenderer> ().enabled = true;
+		}
+	}
+}
