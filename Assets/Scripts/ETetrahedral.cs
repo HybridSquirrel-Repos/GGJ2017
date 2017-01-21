ac24e89286d40585c3a5f833f0683adcabda6e6f
Alexander Björkman <ab3.git@eit.se>
2017-01-21 10:17:55 +0100

    Tetras

---
+++ Assets/Scripts/ETetrahedral.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETetrahedral : MonoBehaviour {
+
+	void Start ()
+    {
+        GetComponent<MeshFilter>().mesh = TetrahedralFactory.mesh;
+    }
+}
