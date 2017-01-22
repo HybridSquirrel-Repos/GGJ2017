b6752ca232f1e3be26a7b15291d626250a065aa9
David Wolters <david@Dags-MacBook-Pro.local>
2017-01-22 10:13:12 +0100

    Almoste there...

--- Assets/Scripts/Object_FadeAway.cs
+++ Assets/Scripts/Object_FadeAway.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Object_FadeAway : MonoBehaviour
 {
-	public float timeUntilFadeStart = 0f;
 	public float fadeSpeed = 0.001f;
 
 	public float alpha = 1;
@@ -12,7 +11,6 @@ public class Object_FadeAway : MonoBehaviour
 
 	Material mat;
 
-	float timer = 0f;
 
 	// Use this for initialization
 	void Start () 
@@ -22,22 +20,17 @@ public class Object_FadeAway : MonoBehaviour
 		c.a = 1;
 		mat.color = c;
 //		print ("Hello there!");
-		timer = timeUntilFadeStart;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (timer <= 0)
-		{
-			/* Start fading */
-			Color color = mat.color;
-			color.a = Mathf.Lerp (color.a, 0, fadeSpeed);
-			mat.color = color;
-			alpha = color.a;
-		} else
-		{
-			timer -= Time.deltaTime;
-		}
+
+		/* Start fading */
+		Color color = mat.color;
+		color.a = Mathf.Lerp (color.a, 0, fadeSpeed);
+		mat.color = color;
+		alpha = color.a;
+	
 	}
 }
